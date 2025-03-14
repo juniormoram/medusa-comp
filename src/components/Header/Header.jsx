42fe26a5d6fbf7662bd63f967235da118711b386
Junior Mora <100890725+juniormoram@users.noreply.github.com>
2025-03-13 18:43:28 -0600

    Charges

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header(){
     return (
         <header className="header">
-        <h2>Medusa Comp</h2>        
+        <h4>MedusaComp</h4>        
         <nav className="navigation">
             <ul>
             <li><a href="/">Inicio</a></li>
